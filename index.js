70ec5fb3d9480c61f8da2486d0ad7c1a17f948d5
brunomry <brunomadozzo@gmail.com>
2024-03-11 05:06:29 -0300

    Agregue midleware para cargar index.html como archivo estatico

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 
@@ -15,3 +17,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "/public")))
