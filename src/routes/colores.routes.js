1201ff0e721f6803a913085cbb95875b954df64e
brunomry <brunomadozzo@gmail.com>
2024-03-15 01:49:50 -0300

    Agregue ruta e invoque la funcion de obtener color por su id mediante solicitud get

--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { crearColor, listarColores } from "../controllers/colores.controllers.js";
+import { crearColor, listarColores, obtenerColor } from "../controllers/colores.controllers.js";
 
 const enrutador = Router();
 
 enrutador.route("/colores").get(listarColores).post(crearColor);
+enrutador.route("/color/:id").get(obtenerColor);
 
 export default enrutador;
